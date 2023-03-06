4890e8142b8d3408746ef6331a17c8f7205d9eae
Ryutaro-y <liverpool.ryutaro@gmail.com>
2023-03-06 17:56:50 +0900

    supplierマスタの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_06_013352) do
+ActiveRecord::Schema.define(version: 2023_03_06_081952) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,13 @@ ActiveRecord::Schema.define(version: 2023_03_06_013352) do
     t.integer "stage"
     t.string "location_name"
     t.index ["user_id"], name: "index_stocks_on_user_id"
+  end
+
+  create_table "suppliers", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
