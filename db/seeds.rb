1056c46d0b75c2463eb1f1023bb4415a66f0cde2
Ryutaro-y <liverpool.ryutaro@gmail.com>
2023-03-07 11:08:45 +0900

    csvでlocationのseedを作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,3 +3,7 @@ require 'csv'
 CSV.foreach('db/csv/inks.csv', headers: true) do |row|
     Ink.create!(row.to_hash)
 end
+
+CSV.foreach('db/csv/locations.csv', headers: true) do |row|
+    Location.create!(row.to_hash)
+end
